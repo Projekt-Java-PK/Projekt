11be63dd8fce39a79688e926e48f88d27b2c31ae
Gladias <alek.drozdz@gmail.com>
2020-06-03 22:55:30 +0200

    Change website to mapfling.com and make loading function

--- Projekt_java_2020/src/Geolocation.java
+++ Projekt_java_2020/src/Geolocation.java
@@ -1,8 +1,13 @@
 import javafx.application.Application;
+import javafx.concurrent.Worker;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 
 public class Geolocation extends Application {
@@ -13,16 +18,37 @@ public class Geolocation extends Application {
 
     @Override
     public void start(Stage stage) {
-        connectOSM(stage);
+        connect(stage);
     }
 
-    private void connectOSM(Stage stage) {
-        String url = "https://www.openstreetmap.org/#map=11/50.0439/19.9422";
 
-        WebView webView = new WebView();
-        webView.getEngine().load(url);
+    private void connect(Stage stage) {
+        String url = "https://mapfling.com/qbx7ezn";
 
-        VBox vBox = new VBox(webView);
+        WebView view = new WebView();
+        WebEngine engine = view.getEngine();
+        engine.load(url);
+
+        // Waiting for page to load
+        engine.getLoadWorker().stateProperty().addListener((observable, oldState, newState) -> {
+            if (newState == Worker.State.SUCCEEDED) {
+                System.out.println("Connected");
+                removePageClutter(stage, view);
+                //webDocument = engine.getDocument();
+            }
+        });
+
+        // if (webDocument.getElementById("sidebdsadzadaar") == null)
+        //    System.out.println("taa");
+        //Element element = webDocument.getElementById("sidebar");
+        //Element element = webDocument.getElementById("sidebar");
+
+
+    }
+
+    private void removePageClutter(Stage stage, WebView view) {
+
+        VBox vBox = new VBox(view);
         Scene scene = new Scene(vBox, 960, 600);
 
         stage.setTitle("Map");
