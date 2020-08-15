0ab342a023d90aead9a83f74a5d7991b9970ce61
Gladias <alek.drozdz@gmail.com>
2020-08-15 18:15:28 +0200

    Update menu to link to proper scenes

--- src/main/java/com.javapk/MenuController.java
+++ src/main/java/com.javapk/MenuController.java
@@ -1,5 +1,6 @@
 package com.javapk;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -7,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MenuController {
@@ -27,6 +29,33 @@ public class MenuController {
         Stage stage = getCurrentStage();
         stage.setScene(new Scene(addEntryLayout, 1280, 720));
     }
+
+    public void displayEntries() throws IOException {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Wybierz folder z wpisami dziennika");
+
+        DisplayEntriesController.selectedDirectory = directoryChooser.showDialog(getCurrentStage());
+
+        Parent displayEntriesLayout = FXMLLoader.load(getClass().getClassLoader().getResource("displayEntries.fxml"));
+        Stage stage = getCurrentStage();
+        stage.setScene(new Scene(displayEntriesLayout, 1280, 720));
+    }
+
+    public void displayPhotos() throws IOException {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Wybierz folder z wpisami dziennika");
+
+        DisplayPhotosController.selectedDirectory = directoryChooser.showDialog(getCurrentStage());
+
+        Parent displayEntriesLayout = FXMLLoader.load(getClass().getClassLoader().getResource("displayPhotos.fxml"));
+        Stage stage = getCurrentStage();
+        stage.setScene(new Scene(displayEntriesLayout, 1280, 720));
+    }
+    /*
+    public void displayMap() {
+        Stage stage = getCurrentStage();
+        stage.setScene(MapDisplay.getMapScene());
+    }*/
 
     public void exitApplication() {
         Stage stage = getCurrentStage();
